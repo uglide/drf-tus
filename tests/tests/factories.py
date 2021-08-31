3b9766c2b2aae89b36ef6feb7763a430e08915f9
Kirill Malovitsa <ciklop.kirill@gmail.com>
2021-08-31 10:55:40 +0500

    Fixed factoryboy DjangoModelFactory

--- tests/tests/factories.py
+++ tests/tests/factories.py
@@ -6,6 +6,6 @@ import factory
 from rest_framework_tus.models import get_upload_model
 
 
-class UploadFactory(factory.DjangoModelFactory):
+class UploadFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = get_upload_model()
