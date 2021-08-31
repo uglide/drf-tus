3174cabb1f4c0cb14eca19cbd9a58b9035e3ff53
Kirill Malovitsa <ciklop.kirill@gmail.com>
2021-08-31 11:05:32 +0500

    Reorganize app_name

--- rest_framework_tus/urls.py
+++ rest_framework_tus/urls.py
@@ -4,10 +4,11 @@ from rest_framework_tus.views import UploadViewSet
 
 from .routers import TusAPIRouter
 
+app_name = 'rest_framework_tus'
+
 router = TusAPIRouter()
 router.register(r'files', UploadViewSet, basename='upload')
 
 urlpatterns = [
     re_path(r'', include((router.urls, 'rest_framework_tus'), namespace='api'))
 ]
-app_name = 'rest_framework_tus'
