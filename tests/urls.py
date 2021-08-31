ddd7031f970903deba034a7d446a94747854e54a
Kirill Malovitsa <ciklop.kirill@gmail.com>
2021-08-31 11:06:06 +0500

    Fixed test requirements

--- tests/urls.py
+++ tests/urls.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8
 from __future__ import unicode_literals, absolute_import
 
-from django.conf.urls import url, include
+from django.urls import re_path, include
 
 from rest_framework_tus.urls import urlpatterns as rest_framework_tus_urls
 
 urlpatterns = [
-    url(r'^', include(rest_framework_tus_urls, namespace='rest_framework_tus')),
+    re_path(r'^', include((rest_framework_tus_urls, 'rest_framework_tus'), namespace='rest_framework_tus')),
 ]
