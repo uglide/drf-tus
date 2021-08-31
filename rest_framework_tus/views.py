f8479925f66cf8d09bcc8b9f3b80654dccc31b9c
Kirill Malovitsa <ciklop.kirill@gmail.com>
2021-08-31 11:05:42 +0500

    Fixed ugettext_lazy

--- rest_framework_tus/views.py
+++ rest_framework_tus/views.py
@@ -6,7 +6,7 @@ import logging
 
 from django.http import Http404
 from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from rest_framework import mixins, status
 from rest_framework.exceptions import MethodNotAllowed
 from rest_framework.metadata import BaseMetadata
